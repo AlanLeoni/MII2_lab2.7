d5f9aeef5de32195fc94874853034ce143408e43
Alan <alan.leoni@usi.ch>
2021-12-01 08:57:29 +0100

    Seconda versione migliorata

--- es.1/bande_di_colori.py
+++ es.1/bande_di_colori.py
@@ -4,15 +4,12 @@ from img_lib_v0_51 import (
     affianca,
     affianca_verticale,
     visualizza_immagine,
-    altezza_immagine,
-    Image,)
-
-from img_util import (
-    affianca_molte,)
-
+    Image,
+)
+PASSO = 32
 
 def crea_quadratino(red: int, green: int, blue: int) -> Image:
-    DIMENSIONE_CELLA =  10
+    DIMENSIONE_CELLA = 10
     colore = "rgb(" + str(red) + "," + str(green) + "," + str(blue) + ")"
     cella = rettangolo(DIMENSIONE_CELLA, DIMENSIONE_CELLA, colore)
     return cella
@@ -20,35 +17,36 @@ def crea_quadratino(red: int, green: int, blue: int) -> Image:
 
 def crea_riga(red: int, green: int, blue: int) -> Image:
     quadratino_prec = immagine_vuota()
-    for i in range(0, 255, 32):
+    for blue in range(0, 255, PASSO):
         quadratino_succ = crea_quadratino(red, green, blue)
         riga = affianca(quadratino_prec, quadratino_succ)
         quadratino_prec = riga
-        blue = blue + 32
     return riga
 
 
-def crea_quadrato(red: int) -> Image:
-    green = 0
-    blue = 0
+def crea_quadrato(red: int, green: int, blue: int) -> Image:
     img_prec = immagine_vuota()
-    for i in range(0, 255, 32):
+    
+    for green in range(0, 255, PASSO):
         nuova_immagine = affianca_verticale(img_prec, crea_riga(red, green, blue))
         img_prec = nuova_immagine
-        green = green + 32
     return nuova_immagine
 
+
+def affianca_molte(immagini_con_spazio: list) -> Image:
+    affiancate = immagine_vuota()
+    for immagine in immagini_con_spazio:
+        affiancate = affianca(affiancate, immagine)
+    return affiancate
+
     
-def crea_slice_quadrati(red = 0) -> Image:
+def crea_slice_quadrati() -> Image:    
     green = 0
     blue = 0
     slice = []
-    for i in range(0, 255, 32):
-        slice_quadrato = crea_quadrato(red)
-        spazio = rettangolo((altezza_immagine(slice_quadrato))//16, altezza_immagine(slice_quadrato), (000, 000,000,0))
-        quadrato_con_spazio = affianca(slice_quadrato,spazio) 
-        red = red + 32
-        slice.append(quadrato_con_spazio)
+    for red in range(0, 255, PASSO):
+        slice_quadrato = crea_quadrato(red, green, blue)
+        slice.append(slice_quadrato)
     img_slice = affianca_molte(slice)
     return img_slice
 
